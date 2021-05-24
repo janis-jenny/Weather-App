d07ad0a7a12131ca4fdf7d3f19479bb656ed96fe
janis-jenny <pcardenasvm@gmail.com>
2021-05-23 21:13:51 -0500

    move renderBackground to another file and fix more linters

--- src/components/render.js
+++ src/components/render.js
@@ -88,27 +88,5 @@ class Render {
     p.className = 'temp-container';
     this.content.append(p);
   }
-  /* renderBackground() {
-    const weather = this.data.weather[0].main.toLowerCase();
-    const imgs = document.getElementById('body');
-    imgs.style.backgroundImage = "url('../images/background.jpg')";
-    if (weather === 'rain' || weather === 'light rain' || weather === 'shower rain') {
-      imgs.style.backgroundImage = "url(rain.gif)";
-    } else if (weather === 'scattered clouds' || weather === 'broken clouds' || weather === 'clouds') {
-      imgs.style.backgroundImage = 'url(cloud.gif)';
-    } else if (weather === 'few clouds') {
-      imgs.style.backgroundImage = 'url(few-clouds.gif)';
-    } else if (weather === 'clear sky' || weather === 'clear') {
-      imgs.style.backgroundImage = 'url(sky.gif)';
-    } else if (weather === 'thunderstorm') {
-      imgs.style.backgroundImage = 'url(thunder.gif)';
-    } else if (weather === 'snow') {
-      imgs.style.backgroundImage = 'url(snow.gif)';
-    } else if (weather === 'mist') {
-      imgs.style.backgroundImage = 'url(mist.gif)';
-    } else {
-      imgs.style.backgroundImage = "url('../images/background.jpg')";
-    }
-  } */
 }
 export default Render;
